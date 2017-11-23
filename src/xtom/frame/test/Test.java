c90e37a0b290d2402d0501d76507110560b8edc4
fuquankai <fuquankai@hemaapp.com>
2017-11-23 16:33:44 +0800

    commit

---
+++ src/xtom/frame/test/Test.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014 The Android Client Of QK Project
+ * 
+ *     The BeiJing PingChuanJiaHeng Technology Co., Ltd.
+ * 
+ * Author:Yang ZiTian
+ * You Can Contact QQ:646172820 Or Email:mail_yzt@163.com
+ */
+package xtom.frame.test;
+
+import xtom.frame.XtomActivity;
+import xtom.frame.net.XtomNetTask;
+
+/**
+ * 
+ */
+public class Test extends XtomActivity {
+
+	@Override
+	protected boolean onKeyBack() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean onKeyMenu() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void findView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void getExras() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void callBeforeDataBack(XtomNetTask netTask) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void callAfterDataBack(XtomNetTask netTask) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void callBackForGetDataSuccess(XtomNetTask netTask, Object result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void callBackForGetDataFailed(int type, XtomNetTask netTask) {
+		// TODO Auto-generated method stub
+	}
+
+}
